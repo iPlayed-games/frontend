edda8ebe58cb629524374759523297c23b6a4bd5
Dennis Littlefield <Littlefield.Dennis@outlook.com>
2023-08-03 15:43:11 -0400

    chore:20-install-mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-
+import { Animated, Basic, bounce, Combined } from '../styles/styles'
 
 
 export default function Home() {
   return (
     <main id='MainElement'>
       <h1>This is a page</h1>
+      <Animated animation={bounce}>Let's bounce.</Animated>
     </main>
   )
 }
