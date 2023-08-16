87a9ed9746a960551adc241f3be7e2b07466df2c
Dennis Littlefield <Littlefield.Dennis@outlook.com>
2023-08-16 13:14:50 -0400

    chore: 38 hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-'use client'
+'use client';
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <main id="MainElement">
+      <Hero />
+
       <h1>This is a page</h1>
     </main>
   )
---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>hero</div>
+  )
+}
+
+export default Hero
