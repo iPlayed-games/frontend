c521f0cf30c2b9ee6436599c1059da398bbc2429
Dennis Littlefield <Littlefield.Dennis@outlook.com>
2023-08-21 10:28:13 -0400

    chore: 38 hero and leaderboard- in prog glow effect in css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
           <div className={styles.caption}>
           <h1 className={styles.title}>Isn’t it time you were rewarded for your epic skills?</h1>
           <p className={styles.text}>Earn badges, show off your gaming prowess, and join a community where your achievements are celebrated like never before. It's time to level up, unlock bragging rights, and embrace the glory at iPlayed!</p>
-          <button className={styles.button}>Log In</button>
+            <button className={styles.buttonNeonPink}>Log In</button>
           </div>
         </div>
         <div className={styles.col}>
