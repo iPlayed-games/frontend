e4c0153828530b44973bd3f3cced30716ac757a4
anivm01 <anivm.webdev@gmail.com>
2023-08-21 11:26:03 -0400

    feat: clean searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -4,39 +4,38 @@ import Autocomplete from '@mui/material/Autocomplete'
 import Stack from '@mui/material/Stack'
 import top100Films from '../tempData'
 import SearchIcon from '@mui/icons-material/Search'
-import { InputAdornment } from '@mui/material'
+import { InputAdornment, backdropClasses, useAutocomplete } from '@mui/material'
+import Input from '@mui/material/Input'
+import InputLabel from '@mui/material/InputLabel'
+import List from '@mui/material/List'
+import styles from '../styles/Searchbar.module.css'
 
 interface SearchbarProps {}
 
 const Searchbar: FC<SearchbarProps> = ({}) => {
+  const { getRootProps, getInputLabelProps, getInputProps, getListboxProps, getOptionProps, groupedOptions } =
+    useAutocomplete({
+      id: 'use-autocomplete-demo',
+      options: top100Films,
+      getOptionLabel: (option) => option.label,
+    })
+
   return (
-    <Stack sx={{ width: 300, backgroundColor: '#fafafa', py: '.5rem', px: '1rem' }}>
-      <Autocomplete
-        freeSolo
-        id="free-solo-demo"
-        disableClearable
-        options={top100Films.map((option) => option.label)}
-        renderInput={(params) => {
-          return (
-            <TextField
-              {...params}
-              variant="standard"
-              margin="none"
-              fullWidth
-              size="medium"
-              InputProps={{
-                ...params.InputProps,
-                type: 'search',
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          )
-        }}
-      />
+    <Stack sx={{ backgroundColor: '#fafafa' }}>
+      <div className={styles.searchbox} {...getRootProps()}>
+        <SearchIcon />
+        {/* <InputLabel {...getInputLabelProps()}>useAutocomplete</InputLabel> */}
+        <input className={styles.input} placeholder="Search..." {...getInputProps()} />
+      </div>
+      {groupedOptions.length > 0 ? (
+        <List {...getListboxProps()}>
+          {(groupedOptions as typeof top100Films).map((option, index) => (
+            <li key={index} {...getOptionProps({ option, index })}>
+              {option.label}
+            </li>
+          ))}
+        </List>
+      ) : null}
     </Stack>
   )
 }
