d2423192565483608b884808bfc0b06f5172bfb9
Dennis Littlefield <Littlefield.Dennis@outlook.com>
2023-08-22 20:47:27 -0400

    fix: 38 hero, cleaned code

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,39 +5,32 @@ import React from 'react'
 //import Leaderboard from './Leaderboard'
 import styles from './Hero.module.css'
 
-
 interface content {
   title: string
   description: string
 }
 
-function createContent(name: string, description: string): Data {
-  return { name, description }
+function createContent(title: string, description: string): content {
+  return { title, description }
 }
 const content = {
   title: "Isn't it time you were rewarded for your epic skills?",
-  description: "Earn badges, show off your gaming prowess, and join a community where your achievements are celebrated like never before.  It's time to level up, unlock bragging rights, and embrace the glory at iPlayed!" ,
+  description:
+    "Earn badges, show off your gaming prowess, and join a community where your achievements are celebrated like never before.  It's time to level up, unlock bragging rights, and embrace the glory at iPlayed!",
 }
-
 
 const Hero = () => {
   return (
-
-      <div className={styles.hero}>
-        <div className={styles.col}>
-          <div className={styles.caption}>
-          <h1 className={styles.title}>
-            {content.title}
-            </h1>
-            <p className={styles.text}>
-              {content.description}
-            </p>
-            {/* TODO:<button className={styles.buttonNeonPink}>Log In</button> */}
-          </div>
+    <div className={styles.hero}>
+      <div className={styles.col}>
+        <div className={styles.caption}>
+          <h1 className={styles.title}>{content.title}</h1>
+          <p className={styles.text}>{content.description}</p>
+          {/* TODO:<button className={styles.buttonNeonPink}>Log In</button> */}
         </div>
-        <div className={styles.col}>{/*TODO: <Leaderboard /> */}</div>
       </div>
-
+      <div className={styles.col}>{/*TODO: <Leaderboard /> */}</div>
+    </div>
   )
 }
 
