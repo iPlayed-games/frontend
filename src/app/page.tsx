350960f6242080ae01f445d8667cc92d5f5556ec
Dennis Littlefield <Littlefield.Dennis@outlook.com>
2023-08-23 11:10:16 -0400

    feat: 38 hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero/Hero'
 
 export default function Home() {
   return (
-    <main id="MainElement">
+    <main>
       <Hero />
     </main>
   )
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,35 +1,21 @@
 'use client'
 
-import React from 'react'
-//FIXME: Leaderboard component needs repair
-//import Leaderboard from './Leaderboard'
 import styles from './Hero.module.css'
 
-interface content {
-  title: string
-  description: string
-}
-
-function createContent(title: string, description: string): content {
-  return { title, description }
-}
-const content = {
-  title: "Isn't it time you were rewarded for your epic skills?",
-  description:
-    "Earn badges, show off your gaming prowess, and join a community where your achievements are celebrated like never before.  It's time to level up, unlock bragging rights, and embrace the glory at iPlayed!",
-}
+const HERO_TITLE: string = "Isn't it time you were rewarded for your epic skills?"
+const HERO_DESCRIPTION: string =
+  "Earn badges, show off your gaming prowess, and join a community where your achievements are celebrated like never before.  It's time to level up, unlock bragging rights, and embrace the glory at iPlayed!"
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
       <div className={styles.col}>
         <div className={styles.caption}>
-          <h1 className={styles.title}>{content.title}</h1>
-          <p className={styles.text}>{content.description}</p>
-          {/* TODO:<button className={styles.buttonNeonPink}>Log In</button> */}
+          <h1 className={styles.title}>{HERO_TITLE}</h1>
+          <p className={styles.text}>{HERO_DESCRIPTION}</p>
         </div>
       </div>
-      <div className={styles.col}>{/*TODO: <Leaderboard /> */}</div>
+      <div className={styles.col}>{/* Leaderboard Component */}</div>
     </div>
   )
 }
