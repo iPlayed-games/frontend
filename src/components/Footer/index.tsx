28727f27aa0115e6022e52737cf0204be437a1e1
Sergio Silva <sergiobtos@hotmail.com>
2023-08-28 15:33:57 -0700

    Test 3

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,10 @@ import { FooterContainer, Container, Copyright } from './styles'
 
 const Footer = () => {
   return (
-    <FooterContainer>      <Container>        <Logo />        <Copyright>Copyright 2023 iPlayed Inc. All rights reserved</Copyright>
+    <FooterContainer>
+      <Container>
+        <Logo /> 
+        <Copyright>Copyright 2023 iPlayed Inc. All rights reserved</Copyright>
       </Container>
     </FooterContainer>
   )
