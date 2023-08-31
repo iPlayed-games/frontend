38e493f0dc7f3d9f4519912e98433cbae928dbb3
Sergio Silva <sergiobtos@hotmail.com>
2023-08-31 16:37:29 -0700

    Adding GlobalStyle from MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@components'
+import GlobalStyles from 'styles/GlobalStyles'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <GlobalStyles />
     </main>
   )
 }
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,34 @@
+import MuiGlobalStyles, { GlobalStylesProps } from '@mui/material/GlobalStyles'
+import { Roboto } from 'next/font/google'
+import { $whiteColor } from './_variables'
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
+
+const STYLES: GlobalStylesProps['styles'] = {
+  '*, *::before, *::after': {
+    boxSizing: 'border-box',
+    shapeRendering: 'geometricPrecision',
+    textRendering: 'geometricPrecision',
+    fillRule: 'evenodd',
+    clipRule: 'evenodd',
+  },
+  'html, body': {
+    fontFamily: `${roboto.style.fontFamily}, sans-serif`,
+    color: $whiteColor,
+    height: '100%',
+    scrollBehavior: 'smooth',
+  },
+  'ol, ul': {
+    listStyle: 'none',
+  },
+}
+
+const GlobalStyles = () => {
+  return <MuiGlobalStyles styles={STYLES} />
+}
+
+export default GlobalStyles
