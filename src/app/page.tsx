532db6f4653b695611ca1dc012742235d1e127e6
Dennis Littlefield <dennis.ah.littlefield@gmail.com>
2023-09-06 18:48:35 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,9 @@ import { StyledCard } from 'common/StyledCard'
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Hero />
-
       <StyledCard>Child Text Passed In From Above</StyledCard>
     </main>
-
-    </>
-
   )
 }
